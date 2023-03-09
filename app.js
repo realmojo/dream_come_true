945460e7f3e1a8b0bc39aa7b3eb4536aa30b65b5
realmojo88 <strikers1999@naver.com>
2023-03-09 21:47:10 +0900

    up

--- app.js
+++ app.js
@@ -9,16 +9,79 @@ const doc = new GoogleSpreadsheet(
   "1HaNcq2TJrbjchZDumxiBuOMD7sG_u_7CFivNP2jDZBI"
 );
 
-// const menus = 734;
-const menus = 2;
-const pages = 100;
+const menus = 734;
+// const menus = 1;
+
+const getCategory = async (driver) => {
+  try {
+    _category = await driver.findElement(By.className("breadcrumb")).getText();
+    _category = _category
+      .replace("\n", " / ")
+      .replace("\n", " / ")
+      .replace("\n", " / ")
+      .replace("\n", " / ");
+  } catch {
+    _category = "-";
+  }
+
+  return _category;
+};
+
+const getTitles = async (driver) => {
+  let _titles = [];
+  try {
+    let t = await driver.findElements(By.className("title"));
+    for (const item of t) {
+      let d = await item.getText();
+      d = d.replace("ㆍ", "");
+      _titles.push(d.trim());
+    }
+  } catch (e) {
+    _titles = [];
+  }
+  return _titles;
+};
+
+const getContents = async (driver) => {
+  let _contents = [];
+  try {
+    let t = await driver.findElements(By.className("content"));
+    for (const item of t) {
+      let d = await item.getText();
+      _contents.push(d.trim());
+    }
+  } catch (e) {
+    _contents = [];
+  }
+  return _contents;
+};
+
+const getInfos = async (driver) => {
+  let _category = await getCategory(driver);
+  let _titles = await getTitles(driver);
+  let _contents = await getContents(driver);
+
+  return {
+    _category,
+    _titles,
+    _contents,
+  };
+};
+
+const insertSheet = async (sheet, driver, _category, _titles, _contents) => {
+  for (let i in _titles) {
+    let t = {
+      _category,
+      _title: _titles[i],
+      _content: _contents[i],
+    };
+    console.log(t);
+    await sheet.addRow(t);
+    await driver.sleep(500);
+  }
+};
 
 const run = async () => {
-  // const service = new chrome.ServiceBuilder("./chromedriver").build();
-  // console.log(service);
-  // chrome.setDefaultService(service);
-  // const driver = await new webdriver.Builder().forBrowser("chrome").build(); // await doc.useServiceAccountAuth(gs_cred);
-
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
 
   await doc.useServiceAccountAuth(gs_cred);
@@ -29,69 +92,37 @@ const run = async () => {
     await driver.get(`https://xn--cw0bv1s99jy7m28a.com/${i}`);
     await driver.sleep(1000);
 
-    let _category = "";
-    try {
-      _category = await driver
-        .findElement(By.className("breadcrumb"))
-        .getText();
-      _category = _category
-        .replace("\n", " / ")
-        .replace("\n", " / ")
-        .replace("\n", " / ")
-        .replace("\n", " / ");
-    } catch {
-      _category = "-";
-    }
+    let { _category, _titles, _contents } = await getInfos(driver);
+    await insertSheet(sheet, driver, _category, _titles, _contents);
 
-    let _titles = [];
     try {
-      let t = await driver.findElements(By.className("title"));
-      for (const item of t) {
-        let d = await item.getText();
-        d = d.replace("ㆍ", "");
-        _titles.push(d.trim());
+      let t = await driver.findElements(By.className("pagination"));
+      if (t.length > 0) {
+        let page = 2;
+        let isPage = true;
+        while (isPage) {
+          await driver.sleep(1000);
+          await driver.get(
+            `https://xn--cw0bv1s99jy7m28a.com/${i}/page-${page}`
+          );
+          await driver.sleep(1000);
+
+          let { _category, _titles, _contents } = await getInfos(driver);
+          await insertSheet(sheet, driver, _category, _titles, _contents);
+
+          let _t = await driver.findElements(By.className("disabled"));
+          if (_t.length > 3) {
+            isPage = false;
+            console.log("페이지 끝");
+          } else {
+            page++;
+          }
+        }
       }
     } catch (e) {
-      _titles = "-";
-    }
-    console.log(_titles);
-
-    let _contents = [];
-    try {
-      let t = await driver.findElements(By.className("content"));
-      for (const item of t) {
-        let d = await item.getText();
-        _contents.push(d.trim());
-      }
-    } catch (e) {
-      _contents = "-";
-    }
-
-    console.log(_contents);
-
-    for (let i in _titles) {
-      let t = {
-        _category,
-        _title: _titles[i],
-        _content: _contents[i],
-      };
-      await sheet.addRow(t);
-      sleep(500);
+      console.log(e);
     }
   }
-
-  // await doc.loadInfo(); // loads document properties and worksheets
-  // const sheet = doc.sheetsByIndex[0];
-  // let category = "건강";
-  // let title = "좋아요";
-  // let content = "매우좋아요";
-  // let t = {
-  //   _category: category ? category : "-",
-  //   _title: title ? title : "-",
-  //   _content: content ? content : "-",
-  // };
-  // console.log(t);
-  // await sheet.addRow(t);
 };
 
 run();
