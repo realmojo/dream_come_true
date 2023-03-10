2be2fd46981e12e59aaf7e218b1268020884a3e9
realmojo88 <strikers1999@naver.com>
2023-03-10 17:37:03 +0900

    up

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// const webdriver = require("selenium-webdriver");
-// const { By } = require("selenium-webdriver");
-// const chrome = require("selenium-webdriver/chrome");
 const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 
 const gs_cred = require("./sheet.json");
@@ -10,7 +7,7 @@ const doc = new GoogleSpreadsheet(
 );
 
 const menus = 734;
-// const menus = 1;
+// 568 까지 진행
 
 const getCategory = async (driver) => {
   try {
@@ -87,40 +84,43 @@ const run = async () => {
   await doc.useServiceAccountAuth(gs_cred);
   await doc.loadInfo(); // loads document properties and worksheets
   const sheet = doc.sheetsByIndex[0];
-  for (let i = 1; i < menus; i++) {
+  for (let i = 20; i < menus; i++) {
     await driver.sleep(1000);
     await driver.get(`https://xn--cw0bv1s99jy7m28a.com/${i}`);
     await driver.sleep(1000);
 
-    let { _category, _titles, _contents } = await getInfos(driver);
-    await insertSheet(sheet, driver, _category, _titles, _contents);
+    let l = await driver.findElements(By.className("list-inline"));
+    if (l.length === 0) {
+      let { _category, _titles, _contents } = await getInfos(driver);
+      await insertSheet(sheet, driver, _category, _titles, _contents);
 
-    try {
-      let t = await driver.findElements(By.className("pagination"));
-      if (t.length > 0) {
-        let page = 2;
-        let isPage = true;
-        while (isPage) {
-          await driver.sleep(1000);
-          await driver.get(
-            `https://xn--cw0bv1s99jy7m28a.com/${i}/page-${page}`
-          );
-          await driver.sleep(1000);
+      try {
+        let t = await driver.findElements(By.className("pagination"));
+        if (t.length > 0) {
+          let page = 2;
+          let isPage = true;
+          while (isPage) {
+            await driver.sleep(1000);
+            await driver.get(
+              `https://xn--cw0bv1s99jy7m28a.com/${i}/page-${page}`
+            );
+            await driver.sleep(1000);
 
-          let { _category, _titles, _contents } = await getInfos(driver);
-          await insertSheet(sheet, driver, _category, _titles, _contents);
+            let { _category, _titles, _contents } = await getInfos(driver);
+            await insertSheet(sheet, driver, _category, _titles, _contents);
 
-          let _t = await driver.findElements(By.className("disabled"));
-          if (_t.length > 3) {
-            isPage = false;
-            console.log("페이지 끝");
-          } else {
-            page++;
+            let _t = await driver.findElements(By.className("disabled"));
+            if (_t.length >= 3) {
+              isPage = false;
+              console.log("페이지 끝");
+            } else {
+              page++;
+            }
           }
         }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
     }
   }
 };
